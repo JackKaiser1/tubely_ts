fa771f1854ffcb7eb6cbdffdd0517be384821e9e
jackkaiser1 <jackkaiser78@gmail.com>
2026-03-17 15:39:56 -0400

    moved thumbnail storage to local files

--- src/api/assets.ts
+++ src/api/assets.ts
@@ -1,9 +1,28 @@
 import { existsSync, mkdirSync } from "fs";
-
 import type { ApiConfig } from "../config";
+import path from "node:path";
 
 export function ensureAssetsDir(cfg: ApiConfig) {
   if (!existsSync(cfg.assetsRoot)) {
     mkdirSync(cfg.assetsRoot, { recursive: true });
   }
 }
+
+export function mediaTypeToExt(mediaType: string) {
+  const mediaTypeParts = mediaType.split("/");
+  if (mediaTypeParts.length !== 2) {
+    return ".bin";
+  }
+  return `.${mediaTypeParts[1]}`;
+}
+
+export function getAssetDiskPath(cfg: ApiConfig, fileName: string) {
+  return path.join(cfg.assetsRoot, fileName);
+}
+
+export function getAssetURL(cfg: ApiConfig, fileName: string) {
+  return `http://localhost:${cfg.port}/assets/${fileName}`;
+}
+
+
+
--- src/api/thumbnails.ts
+++ src/api/thumbnails.ts
@@ -2,9 +2,11 @@ import { getBearerToken, validateJWT } from "../auth";
 import { respondWithJSON } from "./json";
 import { getVideo, updateVideo } from "../db/videos";
 import type { ApiConfig } from "../config";
-import type { BunRequest } from "bun";
+import { pathToFileURL, type BunRequest } from "bun";
 import { BadRequestError, NotFoundError, UserForbiddenError } from "./errors";
 import { displayPartsToString } from "typescript";
+import { mediaTypeToExt, getAssetDiskPath, getAssetURL } from "./assets";
+import path from "node:path";
 
 type Thumbnail = {
   data: ArrayBuffer;
@@ -59,23 +61,22 @@ export async function handlerUploadThumbnail(cfg: ApiConfig, req: BunRequest) {
 
   const mediaType = thumbnail.type;
   const data = await thumbnail.arrayBuffer();
-  const dataBuffer = Buffer.from(data);
-  const dataString = dataBuffer.toString("base64");
-  const dataURL = `data:${mediaType};base64,${dataString}`;
-  const video = getVideo(cfg.db, videoId);
 
+  const extension = mediaTypeToExt(mediaType);
+  const fileName = `${videoId}${extension}`;
+  const filePath = getAssetDiskPath(cfg, fileName);
+  const fileURL = getAssetURL(cfg, fileName);
+  Bun.write(filePath, data);
+
+  const video = getVideo(cfg.db, videoId);
   if (!video) {
     throw new BadRequestError("Video Id invalid");
   }
-
   if (video.userID !== userID) {
     throw new UserForbiddenError("User does not own the specified video");
   }
 
-  // videoThumbnails.set(videoId, { mediaType: mediaType, data: data });
-
-  const thumbnailURL = `http://localhost:8091/api/thumbnails/:${videoId}`;
-  video.thumbnailURL = dataURL;
+  video.thumbnailURL = fileURL;
   updateVideo(cfg.db, video);
 
   console.log("uploading thumbnail for video", videoId, "by user", userID);
